830138915e38364078a76864d45c0af2959b8a48
NatanLuz <natandaluz01@gmail.com>
2026-08-12 21:18:54 -0300

    feat: migra footer para React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from './components/About.jsx'
 import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
 import Hero from './components/Hero.jsx'
 import Projects from './components/Projects.jsx'
@@ -16,6 +17,7 @@ function App() {
         <Projects />
         <Contact />
       </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <div className="text-center">
+            <p>
+              <span data-translate="footer.credit">
+                Desenvolvido Por Natan Da Luz
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
